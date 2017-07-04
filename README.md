622976ceda7abc06cb104980913795ce5c021648
Diogo Menezes <diogo.leitao@oi.net.br>
2017-07-04 16:15:37 -0300

    primeiro

---
+++ README.md
@@ -0,0 +1,19 @@
+# Busca por um container em uma Maquina
+
+## Instalação 
+
+```
+chmod +x which_machine.sh
+```
+
+Caso você não possua o sshpass instalado, faça a instalação conforme a linha abaixo
+
+```
+sudo yum install sshpass
+```
+
+## Exemplo:
+
+```
+./which_machine.sh usuario senha container_que_procura na_maquina1 na_maquina2 na_maquina3
+```
---
+++ which_machine.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Busca por um container em uma Maquina
+# Exemplo:
+# chmod +x which_machine.sh
+# ./which_machine.sh usuario senha container_que_procura na_maquina1 na_maquina2 na_maquina3
+
+# caso você não possua o sshpass instalado, faça a instalação conforme a linha abaixo
+# sudo yum install sshpass
+
+USER=$1
+PASS=$2
+CONTAINER=$3
+MACHINES="${@:4}"
+
+for MACHINE in $MACHINES
+do
+   TOTAL=$(sshpass -p $PASS ssh -o StrictHostKeyChecking=no $USER@$MACHINE sudo docker ps | grep $CONTAINER | wc -l)
+   if [ $TOTAL == 1 ]; then
+    echo "Container $CONTAINER na maquina $MACHINE"
+   fi
+done
